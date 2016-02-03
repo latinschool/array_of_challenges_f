068dee8d1ac2058f93b284fbfe8e05ca5dd804c0
Jamie Pilgrim <hello@jamiepilgrim.com>
2016-02-03 11:08:34 -0600

    added challenges to donut dibs

--- array_of_challenges.rb
+++ array_of_challenges.rb
@@ -2,16 +2,27 @@
 	#Sort all the names in this array alphabetically
 	star_students = [ "Kelly", "Henry", "Theo", "Jennifer", "Michelle"]
 
-puts star_students[2]
+	#star_students.sort!
+
+	sorted_star_students = star_students.sort
+
 	#create a new array that has 19 elements of only your name as a string
 		#(hint: There is a way do this with only typing the string once)
 
+ Array.new(19, "Lena")
 
 	#That first array, star_students and this array, brilliant_students, should be one big array!
 		#combine this array with the one above
 		#Then sort the final array in alphabetic order
 
 	brilliant_students = ["Vedant", "Lena", "Zara", "Jessica"]
+
+	brilliant_star_students = star_students.concat brilliant_students
+
+	brilliant_star_students = star_students + brilliant_students
+
+	brilliant_star_students.sort!
+	print brilliant_star_students
 
 	
 	#BONUS
--- donut_dibs.rb
+++ donut_dibs.rb
@@ -3,14 +3,17 @@ donut_box2 = ["Choc Sprinkles", "Choc Sprinkles", "double choc cake", "double ch
 
 
 #There are two boxes of delicious donuts sitting in front of you. They're everyrthing you dreamed of and more. 
-#But there's a problem. We don't know which donut belongs to which one of your classmates!
-
+#But there's a problem. We don't know who the donuts belong to!
 
 
 #Create a hash where donuts are assigned to specific classmates
 
 
-#Return the donut of a specific person
+#Puts the donut of a specific person
+
+#Add another donut classmate pair into your hash
+
+#Find a method that lets you see all the donuts of your hash
 
 
 #Find three hash methods and experiment with them here:
